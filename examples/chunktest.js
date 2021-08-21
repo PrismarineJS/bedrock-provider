0f6266112a1ea04e47d2ae4e2fa721847e59c8a9
extremeheat <extreme@protonmail.ch>
2021-08-21 13:07:04 +0000

    update docs

--- examples/chunktest.js
+++ examples/chunktest.js
@@ -4,6 +4,7 @@ const { WorldProvider } = require('bedrock-provider')
 console.log(require('bedrock-provider'))
 const ChunkColumn = require('bedrock-provider').chunk('bedrock_1.17.10')
 const Block = require('prismarine-block')('bedrock_1.17.10')
+const assert = require('assert')
 
 async function test () {
   // Create a new ChunkColumn
@@ -15,12 +16,9 @@ async function test () {
         // Set a random block ID
         const id = Math.floor(Math.random() * 1000)
         const block = Block.fromStateId(id)
-        // console.log('block', block)
         cc.setBlock({ x, y, z }, block)
         const gotblock = cc.getBlock({ x, y, z })
-        // console.log('Block', gotblock)
-        // console.assert(gotblock.type === block.type && gotblock.type !== 0)
-        if (gotblock.type !== block.type || gotblock.type === 0) throw Error()
+        assert(gotblock.type !== block.type || gotblock.type === 0)
       }
     }
   }
