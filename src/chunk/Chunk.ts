36ed4e5d7499ed86d7f0cf2841bd7944d2dd7b5e
extremeheat <extreme@protonmail.ch>
2022-02-16 02:12:35 -0500

    WorldProvider API refactor

--- src/chunk/Chunk.ts
+++ src/chunk/Chunk.ts
@@ -1,23 +1,5 @@
+import type { BedrockChunk } from 'prismarine-chunk'
 export interface Vec4 { x: number, y: number, z: number, l?: number }
-
-export abstract class IChunkColumn {
-  x: number
-  z: number
-  chunkVersion: number
-  Section: BaseSubChunk
-  sections: BaseSubChunk[]
-  minY: number
-  maxY: number
-  entities
-  addBlockEntity
-  biomes
-  heights
-  newSection: (y) => any
-  getSection: (y) => BaseSubChunk
-  abstract setBlock (vec4, block): void
-  abstract getBlock (vec4): void
-  networkEncodeNoCache
-}
 
 export abstract class BaseSubChunk {
   sectionVersion: number
@@ -29,7 +11,7 @@ export abstract class BaseSubChunk {
 }
 
 // See the Blob docs for details
-export enum StorageType {
+export const enum StorageType {
   LocalPersistence,
   NetworkPersistence,
   Runtime
